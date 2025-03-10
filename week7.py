d0a89ab10b4c67986e5b6487e8a3ef0f60b226e3
Robert Wideman <kj6gcs@gmail.com>
2025-03-10 16:01:23 +0000

    trying to committ

---
+++ week7.py
@@ -0,0 +1,13 @@
+#If-then: makes a decision based on a condition
+#    If your condition is True, run the code attacehd to the if
+#    If your condition is False, do not run the code for the if
+#Unlike loops, if-statements do not repeat by themselves
+
+#If example
+#if condition:
+#    #the code for the if (will run if the condition is true)
+
+#Let's make an if-statement that checks if a number is 5 or not
+num = 5     #put the number 5 into the num variable
+#Make an if-statement, print something if the number is 5
+if num =
