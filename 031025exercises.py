2ef5acaacf6db9ae05986245a7ca2957160a14b3
Robert Wideman <kj6gcs@gmail.com>
2025-03-10 16:44:14 +0000

    completed exercise 1

---
+++ 031025exercises.py
@@ -0,0 +1,15 @@
+#EXERCISES:
+#1.
+
+#create num variable to take an integer as input
+num = int(input("Enter a number: "))
+
+#condition if equal to zero
+if num == 0:
+    print("Equal to zero.")
+#condition if greater than zero
+elif num > 0:
+    print("Greater than zero.")
+#condition if less than zero
+else:
+    print("Less than zero.")
