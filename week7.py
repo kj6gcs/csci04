b4e27827eb3f4c23c93ac1a0badd5f494581d4ac
Robert Wideman <kj6gcs@gmail.com>
2025-03-10 16:15:49 +0000

    did it work?

--- week7.py
+++ week7.py
@@ -7,9 +7,19 @@
 #if condition:
 #    #the code for the if (will run if the condition is true)
 
+#Else-ifs let you check more than one condition and do different things
+# for different conditions
+#if condition:
+#    Thing for first condition
+#elif condition:
+#    Thing for second condition
+
 #Let's make an if-statement that checks if a number is 5 or not
 num = int(input("Type a number! "))     #put the number 5 into the num variable
 
 #Make an if-statement, print something if the number is 5
 if num == 5:
     print("The number is equal to 5!")
+
+if num != 5:
+    print("Not 5.")
